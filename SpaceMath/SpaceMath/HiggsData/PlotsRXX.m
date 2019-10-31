fdb27b25cd5b897a0b18daa2cf3b6eb0deb74e85
valencia <valencia@adrii>
2019-10-31 16:54:14 -0600

    New file

---
+++ SpaceMath/SpaceMath/HiggsData/PlotsRXX.m
@@ -0,0 +1,266 @@
+(* Wolfram Language Package *)
+
+RVone::usage = "\!\(\*
+StyleBox[\"RVone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghZZ_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xlabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"i\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule]  \
+This command evaluates \!\(\*SubscriptBox[\(R\), \(V\)]\) with V=W, \
+Z when there is dependence only on one parameter. The arguments ghtt, ghbb, ghVV are the h-top top, h-bottom bottom, h-VV \
+couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xlabel is used \
+for indicates the axis-X label. Finally, [[i]] stands for confidence level, i=1 (2) indicates 1\[Sigma] (2\[Sigma])"
+
+RGAMone::usage = "\!\(\*
+StyleBox[\"RGAMone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghWW_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"gCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"mCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xlabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"i\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command evaluates \!\(\*SubscriptBox[\(R\), \(\[Gamma]\)]\) when there is dependence only on one parameter. The arguments ghtt, ghbb, ghWW and gCH are the h-top top, h-bottom bottom, h-WW and h-\!\(\*SuperscriptBox[\(H\), \(-\)]\)\!\(\*SuperscriptBox[\(H\), \(+\)]\) \
+couplings. Here, h represents to SM-like Higgs boson. Labels mCH and x \
+indicate the charged scalar mass and the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xlabel is used \
+for indicates the axis-X label. Finally, [[i]] stands for confidence level, i=1 (2) indicates 1\[Sigma] (2\[Sigma])"
+
+RBOTone::usage = "\!\(\*
+StyleBox[\"RBOTone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xlabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"i\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule]  \
+This command evaluates \!\(\*SubscriptBox[\(R\), \(b\)]\)\
+when there is dependence only on one parameter. The arguments ghtt, ghbb are the h-top top \
+and h-bottom bottom couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xlabel is used \
+for indicates the axis-X label. Finally, [[i]] stands for confidence level, i=1 (2) indicates 1\[Sigma] (2\[Sigma])"
+
+RTAUone::usage = "\!\(\*
+StyleBox[\"RTAUone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtautau_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xlabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"i\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule]  \
+This command evaluates \!\(\*SubscriptBox[\(R\), \(\[Tau]\)]\)\ 
+ when there is dependence only on one parameter. The arguments ghtt, ghbb and ghtautau are the h-top top \
+, h-bottom bottom and h-tau tau couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xlabel is used \
+for indicates the axis-X label. Finally, [[i]] stands for confidence level, i=1 (2) indicates 1\[Sigma] (2\[Sigma])"
+
+TableRVone::usage="\!\(\*
+StyleBox[\"TableRVone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghVV_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xstep_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command generates a table of the signal strength \!\(\*SubscriptBox[\(R\), \(V\)]\), with V= W, Z. The arguments ghtt, ghbb and ghVV are the h-top top \
+, h-bottom bottom and h-V V couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xstep is used \
+to indicates the intervales from xmin to xmax. "
+
+TableRGAMone::usage="\!\(\*
+StyleBox[\"TableRGAMone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghWW_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"gCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"mCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xstep_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command generates a table of the signal strength \!\(\*SubscriptBox[\(R\), \(\[Gamma]\)]\). The arguments ghtt, ghbb, ghWW, gCH, gCH and mCH are the h-top top \
+, h-bottom bottom, h-W W, \!\(\*SuperscriptBox[\(ghH\), \(-\)]\)\!\(\*SuperscriptBox[\(H\), \(+\)]\) couplings, while mCH is the charged scalar boson mass that could be to contribute to process. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xstep is used \
+to indicates the intervales from xmin to xmax. "
+
+TableRBOTone::usage="\!\(\*
+StyleBox[\"TableRBOTone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xstep_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command generates a table of the signal strength \!\(\*SubscriptBox[\(R\), \(b\)]\). The arguments ghtt are ghbb are the h-top top \
+and h-bottom bottom couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xstep is used \
+to indicates the intervales from xmin to xmax. "
+
+TableRTAUone::usage="\!\(\*
+StyleBox[\"TableRTAUone\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtautau_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xstep_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command generates a table of the signal strength \!\(\*SubscriptBox[\(R\), \(\[Tau]\)]\). The arguments ghtt are ghbb are the h-top top, \
+h-bottom bottom and h-tau tau couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xstep is used \
+to indicates the intervales from xmin to xmax. "
+
+RXALL::usage="\!\(\*
+StyleBox[\"RXALL\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"[\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtt_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\" \",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghbb_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghZZ_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghWW_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ghtautau_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"gCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"mCH_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"x_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"y_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xmax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ymin_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ymax_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"xlabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"ylabel_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\",\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"PP_\",\nFontWeight->\"Bold\"]\)\!\(\*
+StyleBox[\"]\",\nFontWeight->\"Bold\"]\)\[Rule] This command generates a table of the signal strength \!\(\*SubscriptBox[\(R\), \(\[Tau]\)]\). The arguments ghtt are ghbb are the h-top top, \
+h-bottom bottom and h-tau tau couplings. Here, h represents to SM-like Higgs boson. The label x \
+indicates the parameter to constraint, while xmin and xmax are the \
+initial and final values defined by users and xstep is used \
+to indicates the intervales from xmin to xmax. "
+
+RV::usage="RV[ghtt_,ghbb_,ghVV_,x_,y_,xmin_,xmax_,ymin_,ymax_,xlabel_,ylabel_,xfor_,yfor_,xformin_,xformax_,xforstep_,yformin_,yformax_,yforstep_,PP_][[i]]\[Rule] This command evaluates \!\(\*SubscriptBox[\(R\), \(V\)]\) with V=W, \
+Z when there is dependence on two or more parameters. The arguments ghtt, ghbb, ghVV are the h-top top, h-bottom bottom, h-VV \
+couplings. Here, h represents to SM-like Higgs boson. Labels x and y \
+indicate the parameters to constraint, while xmin (ymin) and xmax (ymax) are the \
+initial and final values defined by users. Argument xlabel (ylabel) is used \
+for indicates the axis-X label (axis-Y label). The arguments xfor (yfor), xformin (yformin), xforstep (yforstep) represent an additional parameter to constraint, namely: initial value, final value and the intervale from xformin (yformin) to xformax (yformax), respectively. Finally, [[i]] stands for confidence level, i=1 (2) indicates 1\[Sigma] (2\[Sigma])".
+
+
+(* ------------------------------------------------------------------------ *)
+
+Begin["`Package`"]
+End[]
+
+Begin["`PlotsRXX`Private`"]
+
+
+End[]
