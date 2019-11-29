a782e231317e9ff261169bcf859e4c5b7f716fa0
valencia <valencia@adrii>
2019-11-29 14:19:37 -0600

    Update

--- SpaceMath/SpaceMath/Miscellaneous/MiscellaneousFunctions.m
+++ SpaceMath/SpaceMath/Miscellaneous/MiscellaneousFunctions.m
@@ -8,27 +8,27 @@ InstalledSpaceMath::usage="Directory SpaceMath"
 
 Options[DeleteSpaceMath]=
 {
-	AutoOverwriteSpaceMathDirectory-> None,
+	AutoDeleteSpaceMathDirectory-> None,
 	InstalledSpaceMath->FileNameJoin[{$UserBaseDirectory, "Applications","SpaceMath"}]
 };
 
 DeleteSpaceMath[OptionsPattern[]]:=
 Module[
  { 
-  packageName, packageDir, OverwriteSM
+  packageSM, packageDirSM, DeleteSM
  },
- packageName = "SpaceMath";
- packageDir = OptionValue[InstalledSpaceMath];
- OverwriteSM="Do you want to delete the SpaceMath package?";
+ packageSM = "SpaceMath";
+ packageDirSM = OptionValue[InstalledSpaceMath];
+ DeleteSM="Do you want to delete the SpaceMath package?";
 
  (* If the package directory already exists, ask the user about overwriting *)
- If[ DirectoryQ[packageDir],
-  If[ OptionValue[AutoOverwriteSpaceMathDirectory],
-   Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
+ If[ DirectoryQ[packageDirSM],
+  If[ OptionValue[AutoDeleteSpaceMathDirectory],
+   Quiet@DeleteDirectory[packageDirSM, DeleteContents -> True],
    Null,
-   If[ ChoiceDialog[OverwriteSM,{"Yes, delete the " <> packageName <>" package"->True,
+   If[ ChoiceDialog[DeleteSM,{"Yes, delete the " <> packageSM <>" package"->True,
     "No, I need to do a backup first. Abort installation."->False}, WindowFloating->True, WindowTitle->"Existing SpaceMath Installation detected"],
-	Quiet@DeleteDirectory[packageDir, DeleteContents -> True],
+	Quiet@DeleteDirectory[packageDirSM, DeleteContents -> True],
 	Abort[]
 	 ]
 	]
