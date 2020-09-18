5697096ba329aaee34acf653761a0902a0d31192
valencia <valencia@adrii>
2020-09-18 13:37:13 -0500

    update

--- SpaceMath/SpaceMath/Miscellaneous/MiscellaneousFunctions.m
+++ SpaceMath/SpaceMath/Miscellaneous/MiscellaneousFunctions.m
@@ -3,6 +3,8 @@
 DeleteSpaceMath::usage="DeleteSpaceMath is an option of InstallSpaceMath. It specifies, the full path \
 to the directory where SpaceMath will be installed.";
 
+UpdateSpaceMathData::usage="Update to current SpaceMath data"
+
 InstalledSpaceMath::usage="Directory SpaceMath"
 
 Begin["`Package`"]
@@ -29,6 +31,47 @@ Module[
  (* If the package directory already exists, ask the user about overwriting *)
  If[ DirectoryQ[packageDirSM],
   If[ OptionValue[AutoDeleteSpaceMathDirectory],
+   Quiet@DeleteDirectory[packageDirSM, DeleteContents -> True],
+   Null,
+   If[
+   	 ChoiceDialog[
+   	 	DeleteSM,
+   	 		{"Yes, delete the " <> packageSM <>" package"->True,
+   	 		 "No, I need it yet. Abort the deletion."->False
+   	 		}, WindowFloating->True],
+	 Quiet@DeleteDirectory[packageDirSM, DeleteContents -> True],
+	 Abort[]
+	 ]
+	]
+   ];
+
+ WriteString["stdout", "\n Package removed! Good bye ... \n"];
+
+];
+
+
+(* 
+Import["https://raw.githubusercontent.com/spacemathproject/SpaceMath/stable/SpaceMath/Install.m"]
+*)
+
+Options[UpdateSpaceMathData]=
+{
+	AutoUpdateSpaceMathData-> None,
+	InstalledSpaceMathData->FileNameJoin[{$UserBaseDirectory, "Applications","SpaceMath","Values"}]
+};
+
+UpdateSpaceMathData[OptionsPattern[]]:=
+Module[
+ { 
+  packageSM, packageDirSM, DeleteSM
+ },
+ packageSM = "SpaceMath";
+ packageDirSM = OptionValue[InstalledSpaceMathData];
+ DeleteSM="Do you want to delete the SpaceMath data current?";
+
+ (* If the data file already exists, ask the user about overwriting *)
+ If[ DirectoryQ[packageDirSM],
+  If[ OptionValue[AutoUpdateSpaceMathData],
    Quiet@DeleteDirectory[packageDirSM, DeleteContents -> True],
    Null,
    If[
