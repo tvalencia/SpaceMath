b2675edb93b3539d56d53bf20f2d521a31401da9
tvalencia <53123066+tvalencia@users.noreply.github.com>
2020-10-07 13:03:29 -0500

    update

---
+++ SpaceMath/build/SpaceMath/PacletInfo.m
@@ -0,0 +1,15 @@
+(* Paclet Info File *)
+
+(* created 2020/09/06*)
+
+Paclet[
+    Name -> "SpaceMath",
+    Version -> "1.0",
+    MathematicaVersion -> "10+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Tutorials/SpaceMathOverview"}
+        }
+]
+
+
