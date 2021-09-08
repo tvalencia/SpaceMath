526ca8bcd3fb87f290f7c42b5e73a3fb2e60a2b4
tvalencia <53123066+tvalencia@users.noreply.github.com>
2021-09-08 11:46:55 -0500

    Update Install.m

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -46,7 +46,7 @@ InstallSpaceMath[]:=
 				  ]
 			  ];
 
-			TempCompressFolder = URLSave["https://github.com/spacemathproject/SpaceMath/archive/stable.zip",CreateTemporary[]];
+			TempCompressFolder = URLSave["https://github.com/spacemathproject/SpaceMath/archive/refs/heads/development.zip",CreateTemporary[]];
 			DecompressTempFolder = TempCompressFolder<>".dir";
 
 			ExtractArchive[TempCompressFolder, DecompressTempFolder];
